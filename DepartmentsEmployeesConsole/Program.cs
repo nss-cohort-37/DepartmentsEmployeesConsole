d96d4c4f55f23621fe8ac2388529087bf8040c4d
Adam Sheaffer <adam.e.sheaffer@gmail.com>
2020-03-20 11:44:01 -0500

    get by id and get all

---
+++ DepartmentsEmployeesConsole/Program.cs
@@ -0,0 +1,26 @@
+﻿using DepartmentsEmployeesConsole.Data;
+using System;
+
+namespace DepartmentsEmployeesConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var repo = new EmployeeRepository();
+            var employees = repo.GetAllEmployees();
+
+            foreach (var employee in employees)
+            {
+                Console.WriteLine($"{employee.FirstName} {employee.LastName} is in {employee.Department.DeptName}");
+            }
+
+
+            Console.WriteLine("Let's get an employee with the ID 2");
+
+            var employeeWithId2 = repo.GetEmployeeById(2);
+
+            Console.WriteLine($"Employee with Id 2 is {employeeWithId2.FirstName} {employeeWithId2.LastName}");
+        }
+    }
+}
